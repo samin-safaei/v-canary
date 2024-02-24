184f9ec71d29d8a58fe892cc34a9e5a61fcd406c
Samin Safaie <s.safaei@hamrahcard.ir>
2024-02-24 15:48:52 +0100

    Added spring boot application and empty properties file

---
+++ src/main/java/com/v/canary/CanaryApplication.java
@@ -0,0 +1,12 @@
+package com.v.canary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanaryApplication.class, args);
+    }
+}
