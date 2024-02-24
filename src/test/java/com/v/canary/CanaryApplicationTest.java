5c5d6ac55f48806f1d42f5bf4545ba3ff40e2345
Samin Safaie <s.safaei@hamrahcard.ir>
2024-02-24 15:51:48 +0100

    Implemented context loading test

---
+++ src/test/java/com/v/canary/CanaryApplicationTest.java
@@ -0,0 +1,13 @@
+package com.v.canary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CanaryApplicationTest extends AbstractIntegrationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
